ac9f5ba5a14a81fc6af90f9650f544e1fc8767e7
haisyamm <haisyammaulana22@gmail.com>
2023-06-14 16:38:16 +0700

    pertemuan 14

--- resources/views/rabs/index.blade.php
+++ resources/views/rabs/index.blade.php
@@ -1,43 +1,50 @@
 @extends('app')
 @section('content')
 @if(session('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong>Hai {{ Auth()->user()->name }}</strong> {{ session('success') }}
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="alert alert-success  alert-dismissible fade show" role="alert">
+  {{ session('success') }}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" 
+  aria-label="Close"></button>
 </div>
 @endif
 <div class="text-end mb-2">
-    <a class="btn btn-light" href="{{ route('departements.exportPdf') }}"> Export</a>
-    <a class="btn btn-secondary" href="{{ route('departements.create') }}"> Add Departement</a>
+<a class="btn btn-secondary" href="{{ route('rabs.create') }}"> Add Departement</a>
 </div>
 <table id="example" class="table table-striped" style="width:100%">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">Name</th>
-      <th scope="col">Location</th>
+      <th scope="col">#</th>        
+      <th scope="col">NO TRX</th>   
+      <th scope="col">Tanggal RAB</th>
       <th scope="col">Manager Name</th>
+      <th scope="col">Jumlah</th>
+      <th scope="col">Total</th>
       <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
-    @foreach ($departements as $item)
-        <tr>
-            <td>{{ $item->id }}</td>
-            <td>{{ $item->name }}</td>
-            <td>{{ $item->location }}</td>
-            <td>{{ (isset($item->getManager->name) ? 
-                    $item->getManager->name : 
-                    'Data Not Found') }}</td>
-            <td>
-                <form action="{{ route('departements.destroy',$item->id) }}" method="Post">
-                    <a class="btn btn-primary" href="{{ route('departements.edit',$item->id) }}">Edit</a>
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>
-        </tr>
+    @foreach ($rabs as $data)
+    <tr>
+        <td>{{ $data->id }}</td>
+        <td>{{ $data->no_rab }}</td>
+        <td>{{ $data->tgl_rab }}</td>
+        <td>{{ 
+          (isset($data->getManager->name)) ? 
+          $data->getManager->name : 
+          'Tidak Ada'
+          }}
+        </td>
+        <td>{{ $data->detail->count() }}</td>
+        <td>{{ $data->total }}</td>
+        <td>
+            <form action="{{ route('rabs.destroy',$data->id) }}" method="Post">
+                <a class="btn btn-primary" href="{{ route('rabs.edit',$data->id) }}">Edit</a>
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </td>
+    </tr>
     @endforeach
   </tbody>
 </table>
